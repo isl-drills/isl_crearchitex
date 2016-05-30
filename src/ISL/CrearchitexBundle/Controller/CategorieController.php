0de56faa15427febf53fe289c82563dc987bc89b
Jeanphinov <novitz@gmail.com>
2016-05-30 16:13:15 +0200

    Categorie - ajout + update

--- src/ISL/CrearchitexBundle/Controller/CategorieController.php
+++ src/ISL/CrearchitexBundle/Controller/CategorieController.php
@@ -8,38 +8,62 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class CategorieController extends Controller
-{
+class CategorieController extends Controller {
+
     /**
      * @Route("/admin/categorie/new", name="categories_ajout")
      */
-    public function newAction(Request $request)
-    {
+    public function newAction(Request $request) {
 
-        $categorie=new Categorie();
-        $form=  $this->createForm(CategorieType::class,$categorie);
-        
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+
         $form->handleRequest($request);
-        
-        if($form->isSubmitted() && $form->isValid()){
-            $em=$this->getDoctrine()->getManager();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
             $em->persist($categorie);
             $em->flush();
-            
+
             return $this->redirectToRoute("home");
         }
-        
-        return $this->render('ISLCrearchitexBundle:admin:categories_form_add.html.twig',array("form"=>$form->createView(),));
+
+        return $this->render('ISLCrearchitexBundle:admin:categories_form_add.html.twig', array("form" => $form->createView(),));
     }
-    
+
+    /**
+     * @Route("/admin/categorie/modify/{id}", name="categories_modifie")
+     */
+    public function modifyAction(Request $request, $id) {
+        $categorie = new Categorie();
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('ISLCrearchitexBundle:Categorie');
+        $categorie=$repo->find($id);
+
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+
+            return $this->redirectToRoute("home");
+        }
+
+        return $this->render('ISLCrearchitexBundle:admin:categories_form_modify.html.twig', array("form" => $form->createView(),));
+    }
+
     /**
      * @Route("/categories/show", name="categories_show")
      */
-    public function showAction(){
-        $doctrine=$this->getDoctrine()->getManager();
-        $repo=$doctrine->getRepository('ISLCrearchitexBundle:Categories');
-        $categories=$repo->findAll();
-        
-        return $this->render('ISLCrearchitexBundle:admin:categories_view.html.twig',["categories"=>$categories]);
+    public function showAction() {
+        $doctrine = $this->getDoctrine()->getManager();
+        $repo = $doctrine->getRepository('ISLCrearchitexBundle:Categories');
+        $categories = $repo->findAll();
+
+        return $this->render('ISLCrearchitexBundle:admin:categories_view.html.twig', ["categories" => $categories]);
     }
+
 }
