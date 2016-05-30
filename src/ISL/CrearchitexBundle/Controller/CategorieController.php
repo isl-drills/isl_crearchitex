48521497a4a386cdf4839153629925d520e6a16f
Jeanphinov <novitz@gmail.com>
2016-05-30 12:15:04 +0200

    Categorie - formumaire ajout

--- src/ISL/CrearchitexBundle/Controller/CategorieController.php
+++ src/ISL/CrearchitexBundle/Controller/CategorieController.php
@@ -6,17 +6,28 @@ use ISL\CrearchitexBundle\Entity\Categorie;
 use ISL\CrearchitexBundle\Form\CategorieType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategorieController extends Controller
 {
     /**
      * @Route("/admin/categorie/new", name="categories_ajout")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
 
         $categorie=new Categorie();
         $form=  $this->createForm(CategorieType::class,$categorie);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+            
+            return $this->redirectToRoute("home");
+        }
         
         return $this->render('ISLCrearchitexBundle:admin:categories_form_add.html.twig',array("form"=>$form->createView(),));
     }
